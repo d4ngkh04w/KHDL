03aee00b763493992983b7c90d1b523f6a92ba65
d4ngkh04w <khoadn1109@gmail.com>
2026-03-14 17:59:44 +0700

    First commit

---
+++ scripts/merge_data.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def read_json(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+data_mobilecity = read_json("../data/json/cellphones_data.json")
+data_cellphones = read_json("../data/json/mobilecity_data.json")
+
+all_products = data_mobilecity + data_cellphones
+
+print(f"Total products: {len(all_products)}")
+
+os.makedirs("../data/raw", exist_ok=True)
+with open("../data/raw/all_products.json", "w", encoding="utf-8") as f:
+    json.dump(all_products, f, ensure_ascii=False, indent=4)
